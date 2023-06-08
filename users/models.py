8e5d9267f6490da75837987973289d45eadd5a86
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-08 13:47:56 +0900

    💬Comment: User 모델 주석 추가 #2

--- users/models.py
+++ users/models.py
@@ -3,6 +3,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """유저 모델
+
+    AbstractUser 유저 모델을 커스텀한 유저 모델입니다.
+
+    Attributes:
+        LoginTypeChoices(class) : 회원가입 유형(일반,카카오,구글,네이버)
+        email (str): 이메일, 필수
+        nickname (str) : 닉네임
+        password (str): 패스워드
+        avatar(str) : 유저의 프로필 사진을 url로 가져옵니다.
+        age(int) : 나이
+        updated_at (date): 수정시간
+        created_at (date): 가입시간
+        login_type (str): 회원가입 유형의 종류를 지정
+        is_active (bool): 활성 여부
+        is_admin (bool): 관리자 여부
+        is_host(bool): 본인 여부
+        followings(list) : 팔로잉 목록
+    """
+
     class LoginTypeChoices(models.TextChoices):
         NORMAL = ("normal", "일반")
         KAKAO = ("kakao", "카카오")
