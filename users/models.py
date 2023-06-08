041da53aa2cad7d2adc1e2c36aa000b990c8db26
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-08 13:02:01 +0900

    ✨Feat: 유저 모델 추가 #2

--- users/models.py
+++ users/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class LoginTypeChoices(models.TextChoices):
+        NORMAL = ("normal", "일반")
+        KAKAO = ("kakao", "카카오")
+        GOOGLE = ("google", "구글")
+        NAVER = ("naver", "네이버")
+
+    email = models.EmailField(
+        max_length=255,
+        default="",
+        unique=False,
+    )
+    nickname = models.CharField(
+        max_length=150,
+        default="",
+    )
+    password = models.CharField(
+        max_length=256,
+    )
+    avatar = models.URLField(
+        blank=True,
+        null=True,
+    )
+    age = models.PositiveIntegerField(
+        null=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    login_type = models.CharField(
+        max_length=15,
+        choices=LoginTypeChoices.choices,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_admin = models.BooleanField(
+        default=False,
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    followings = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True,
+    )
