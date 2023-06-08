55a2b98d5d06c61c94c6ae5741d80ccd635d5ff1
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-08 15:36:27 +0900

    users.urls

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.UsersView.as_view()),
+    path("", views.UserView.as_view()),
+    path("sign-up/", views.UserView.as_view()),
+    path("log-in/", views.UserView.as_view()),
+    path("oauth/kakao/", views.UserView.as_view()),
+    path("oauth/google/", views.UserView.as_view()),
+    path("oauth/naver/", views.UserView.as_view()),
+    path("token/refresh/", views.UserView.as_view(), name="token_refresh"),
+    path("change-password/", views.UserView.as_view()),
+    path("search-password/", views.UserView.as_view()),
+    path("<int:user_id>/", views.UserView.as_view()),
+    path("<int:user_id>/fridge/", views.UserView.as_view()),
+    path("<int:user_id>/avatar/", views.UserView.as_view()),
+    path("<int:user_id>/follow/", views.UserView.as_view()),
 ]
