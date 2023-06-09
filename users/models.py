3ecbced2db63c2995f6a737f6b1ac7d8d88e7540
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-09 19:31:36 +0900

    ingredient

--- users/models.py
+++ users/models.py
@@ -92,7 +92,7 @@ class Fridge(models.Model):
     )
 
     # 06.09 수정 : 나중에 ingredient테이블로 FOREIGN KEY연결해야함
-    ingredient = models.TextField()
+    # ingredient = models.TextField()
 
     def __str__(self):
         return str(self.ingredient)
