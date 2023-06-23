1c50d14a0452788e8daa8a759ba69de22e5992eb
2017250297-choi <97ckdtn@gmail.com>
2023-06-23 21:49:11 +0900

    ✨Feat: 냉장고기반 레시피추천 기능 추가
    - 냉장고기반 레시피 추천기능 추가
    - 기존 사용자협력기반추천기능 오류 수정
    - 자신의 글은 제거하고 보여줌
    -최대 10개 보여줌

--- ai_process/views.py
+++ ai_process/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework import status, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from articles.paginations import ArticlePagination, CommentPagination
+from users.models import Fridge
+from articles.models import (
+    Article,
+    Category,
+    Ingredient,
+    IngredientLink,
+    RecipeIngredient,
+)
+from articles.permissions import IsAuthenticatedOrReadOnlyExceptBookMark
+from django.db.models import Count
+from articles.serializers import (
+    ArticleSerializer,
+    ArticleListSerializer,
+    CategorySerializer,
+    ArticleDetailSerializer,
+    CommentSerializer,
+    IngredientSerializer,
+    RecipeIngredientCreateSerializer,
+    IngredientLinkSerializer,
+    TagSerializer,
+)
+from django.conf import settings
+import requests
+from django.db.models import Q, Count
+from taggit.models import Tag
+from articles.coupang import save_coupang_links_to_ingredient_links
 
-# Create your views here.
+from ai_process.recommend import collaborative_filtering, content_base
+
+
+class TestView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        func_dict = {"0": collaborative_filtering, "1": content_base}
+        select = request.GET.get("recommend", "0")
+        list_of_pk, dictionary = func_dict[select](request.user.id)
+        articles = sorted(
+            Article.objects.filter(id__in=list_of_pk),
+            key=lambda x: dictionary[x.id],
+            reverse=True,
+        )
+        serializer = ArticleListSerializer(
+            articles, many=True, context={"request": request}
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
