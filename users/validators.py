2e88bb0b9505e82d93dbd419dd987129854d0980
hyukjunkim1116 <nmkk1234@naver.com>
2023-07-04 00:16:47 +0900

    🚑 비밀번호 변경 오류 수정 #2

--- users/validators.py
+++ users/validators.py
@@ -1,17 +1,16 @@
 import re
 from django.core.exceptions import ValidationError
+from rest_framework.response import Response
 
 
 def validate_password(password):
     """비밀번호 유효성 검사"""
     if len(password) < 8:
-        raise ValidationError({"password": "비밀번호는 8자리 이상이어야 합니다."})
+        return Response({"error": "비밀번호는 8자리 이상이어야 합니다."})
     if not re.search(r"[a-zA-Z]", password):
-        raise ValidationError({"password": "비밀번호는 하나 이상의 영문이 포함되어야 합니다."})
+        return Response({"error": "비밀번호는 하나 이상의 영문이 포함되어야 합니다."})
     if not re.search(r"\d", password):
-        raise ValidationError({"password": "비밀번호는 하나 이상의 숫자가 포함되어야 합니다."})
+        return Response({"error": "비밀번호는 하나 이상의 숫자가 포함되어야 합니다."})
     if not re.search(r"[!@#$%^&*()]", password):
-        raise ValidationError(
-            {"password": "비밀번호는 적어도 하나 이상의 특수문자(!@#$%^&*())가 포함되어야 합니다."}
-        )
+        return Response({"error": "비밀번호는 적어도 하나 이상의 특수문자(!@#$%^&*())가 포함되어야 합니다."})
     return password
