7d71c6693082792b528b0f0d286ea9620e47c785
hyukjunkim1116 <nmkk1234@naver.com>
2023-07-07 20:02:04 +0900

    divide

--- cookai/urls.py
+++ cookai/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("articles/", include("articles.urls")),
     path("ai_process/", include("ai_process.urls")),
+    path("sentry-debug/", trigger_error),
 ]
