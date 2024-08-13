7a5a302550a87f20ba86070ed518d7cf46f2f82d
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-08-14 00:57:43 +0900

    test: Test Github Action CICD
    Files:
     .github/workflows/deploy_prod.yml cookai/urls.py cookai/views.py

--- cookai/urls.py
+++ cookai/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
     path("users/", include("users.urls")),
     path("articles/", include("articles.urls")),
     path("ai_process/", include("ai_process.urls")),
