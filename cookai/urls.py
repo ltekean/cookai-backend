75eb44e3d58ea23ffd1299bd69e8adee1d43c575
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-08-14 01:22:39 +0900

    test: Test Github Action CICD
    Files:
     cookai/urls.py

--- cookai/urls.py
+++ cookai/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import health_check
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+api_prefix = 'api/v1'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("health", health_check, name="health_check"),
-    path("users/", include("users.urls")),
-    path("articles/", include("articles.urls")),
-    path("ai_process/", include("ai_process.urls")),
+    path(f"{api_prefix}/admin", admin.site.urls),
+    path(f"{api_prefix}/health", health_check, name="health_check"),
+    path(f"{api_prefix}/users", include("users.urls")),
+    path(f"{api_prefix}/articles", include("articles.urls")),
+    path(f"{api_prefix}/ai_process", include("ai_process.urls")),
+    path(api_prefix, include(router.urls)),  # Add prefix here
 ]
