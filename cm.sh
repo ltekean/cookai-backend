a819a3aafc2b531e85259b08d6d3530e57b8368a
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-08-13 22:45:47 +0900

    test: Test Github Action CICD
    Files:
    cm.sh

--- cm.sh
+++ cm.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# 변경된 파일 목록을 가져오기
+CHANGED_FILES=$(git status --porcelain | awk '{print $2}')
+
+# 커밋 메시지를 작성하기 위해 변경된 파일을 리스트로 나열
+COMMIT_MESSAGE="test: Test Github Action CICD
+
+Files:
+$CHANGED_FILES"
+
+# 변경된 파일을 스테이징
 git add .
-git commit -m "test"
+
+# 커밋 실행
+git commit -m "$COMMIT_MESSAGE"
+
+# 푸시 실행
 git push
